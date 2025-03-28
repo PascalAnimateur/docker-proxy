d3de5c648948538276327c43304565696bf2785a
Pascal Martineau <pascal.animateur@gmail.com>
2025-03-28 09:21:04 -0400

    fix: cd into project path when deploying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR/..
+
 echo "🚀 Deploying infrastructure..."
 
 echo "🌐 Creating default network..."
